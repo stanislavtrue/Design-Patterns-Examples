9ce674d01f1f94d82071b2489951a34272c174e3
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-14 12:14:17 +0100

    Update Program.cs

--- Behavioral/Iterator/Program.cs
+++ Behavioral/Iterator/Program.cs
@@ -1,46 +1,56 @@
 ﻿using System;
+
 interface IIterator<T>
 {
     bool MoveNext();
     T next();
 }
+
 interface IPlaylist
 {
     IIterator<Song> CreateIterator();
 }
+
 class Song
 {
     public string Name { get; }
     public string Singer { get; }
+    
     public Song (string singer, string name)
     {
         Singer = singer;        
         Name = name;
     }
 }
+
 class Playlist : IPlaylist
 {
     private List<Song> songs = new List<Song>();
     public void AddSong(Song song) => songs.Add(song);
+    
     public IIterator<Song> CreateIterator()
     {
         return new PlaylistIterator(songs);
     }
 }
+
 class PlaylistIterator : IIterator<Song>
 {
     private readonly List<Song> _songs;
     private int position;
     public PlaylistIterator(List<Song> songs) => _songs = songs;
+    
     public bool MoveNext()
     {
         return position < _songs.Count;
     }
+    
     public Song next()
     {
         return _songs[position++];
     }
 }
+
 class Program
 {
     static void Main(string[] args)
