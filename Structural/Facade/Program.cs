457d975440525d683efa49999bc8e51ed39696b5
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-14 12:04:00 +0100

    Update Program.cs

--- Structural/Facade/Program.cs
+++ Structural/Facade/Program.cs
@@ -1,15 +1,18 @@
 ﻿using System;
+
 class CPU
 {
     public void Freeze()
     {
         Console.WriteLine("Stops running processes...");
     }
+    
     public void Execute()
     {
         Console.WriteLine("Executes commands...");
     }
 }
+
 class HardDrive
 {
     public void Read()
@@ -17,6 +20,7 @@ class HardDrive
         Console.WriteLine("Reads data from disk...");
     }
 }
+
 class RAM
 {
     public void Load()
@@ -24,17 +28,20 @@ class RAM
         Console.WriteLine("Loads data into memory...");
     }
 }
+
 class Facade
 {
     protected CPU cpu;
     protected HardDrive harddrive;
     protected RAM ram;
+    
     public Facade()
     {
         cpu = new CPU();
         harddrive = new HardDrive();
         ram = new RAM();  
     }
+    
     public void StartComputer()
     {
         Console.WriteLine("Starting Computer...");
@@ -44,12 +51,14 @@ class Facade
         cpu.Execute();
         Console.WriteLine("Computer started!\n");
     }
+    
     public void StopComputer() 
     {
         Console.WriteLine("Turning off the computer...");
         Console.WriteLine("Computer is off.");
     }
 }
+
 class Program
 {
     static void Main(string[] args)
