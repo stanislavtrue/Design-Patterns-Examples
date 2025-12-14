7dcecaa1a2591d73bb0778aeb6a5964fd0c1b285
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-14 12:10:12 +0100

    Update Program.cs

--- Creationals/Factory method/Program.cs
+++ Creationals/Factory method/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+
 interface IFigure
 {
     void Draw();
 }
+
 class Circle : IFigure
 {
     public void Draw()
@@ -10,6 +12,7 @@ class Circle : IFigure
         Console.WriteLine("A circle is drawn.");
     }
 }
+
 class Square : IFigure
 {
     public void Draw()
@@ -17,6 +20,7 @@ class Square : IFigure
         Console.WriteLine("A square is drawn.");
     }
 }
+
 class Triangle : IFigure
 {
     public void Draw()
@@ -24,15 +28,18 @@ class Triangle : IFigure
         Console.WriteLine("A triangle is drawn.");
     }
 }
+
 abstract class Painting
 {
     public abstract IFigure CreateFigure();
+    
     public void ChoiceFigure()
     {
         IFigure figure = CreateFigure();
         figure.Draw();
     }
 }
+
 class CircleDraw : Painting
 {
     public override IFigure CreateFigure()
@@ -40,6 +47,7 @@ class CircleDraw : Painting
         return new Circle();
     }
 }
+
 class SquareDraw : Painting
 {
     public override IFigure CreateFigure()
@@ -47,6 +55,7 @@ class SquareDraw : Painting
         return new Square();
     }
 }
+
 class TriangleDraw : Painting
 {
     public override IFigure CreateFigure()
@@ -54,6 +63,7 @@ class TriangleDraw : Painting
         return new Triangle();
     }
 }
+
 class Program
 {
     static void Main(string[] args)
@@ -61,19 +71,16 @@ class Program
         Painting painting = null;
         Console.WriteLine("Enter the shape you want to draw (circle, square, triangle)\n");
         string shape = Console.ReadLine().ToLower();
+        
         if (shape == "circle")
-        {
             painting = new CircleDraw();
-        }
         else if (shape == "square")
-        {
             painting = new SquareDraw();
-        }
         else if (shape == "triangle")
-        {
             painting = new TriangleDraw();
-        }
-        else Console.WriteLine("Unknown figure.");
+        else 
+            Console.WriteLine("Unknown figure.");
+        
         painting?.ChoiceFigure();
     }
 }
