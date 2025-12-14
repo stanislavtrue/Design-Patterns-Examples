c89b2c83b86407f6571d91da2397e58d1612f264
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-14 12:02:32 +0100

    Update Program.cs

--- Structural/Composite/Program.cs
+++ Structural/Composite/Program.cs
@@ -1,45 +1,56 @@
 ﻿using System;
 using System.CodeDom.Compiler;
+
 interface IEmployee
 {
     void ShowDetails(int indent = 0);
 }
+
 class Developer : IEmployee
 {
     private string name;
+    
     public Developer(string name)
     {
         this.name = name;
     }
+    
     public void ShowDetails(int indent = 0)
     {
         Console.WriteLine(new string(' ', indent) + "- Dev: " + name);
     }
 }
+
 class Manager : IEmployee
 {
     private string name;
+    
     public Manager(string name)
     {
         this.name = name;
     }
+    
     public void ShowDetails(int indent = 0)
     {
         Console.WriteLine(new string(' ', indent) + "- HR: " + name);
     }
 }
+
 class Departament : IEmployee
 {
     private string name;
     private List<IEmployee> employees = new List<IEmployee>();
+    
     public Departament(string name)
     {
         this.name = name;
     }
+    
     public void Add(IEmployee employee)
     {
         employees.Add(employee);
     }
+    
     public void ShowDetails(int indent = 0)
     {
         Console.WriteLine(new string(' ', indent) + "- " + name + ":");
@@ -49,6 +60,7 @@ class Departament : IEmployee
         }
     }
 }
+
 class Program
 {
     static void Main(string[] args)
