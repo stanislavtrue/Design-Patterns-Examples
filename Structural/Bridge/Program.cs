27ddb7e57d8e98c333bbfb4dd51116dcac6dc80d
Stanislav Pustovhar <flintbrawl66@gmail.com>
2025-12-14 12:01:43 +0100

    Update Program.cs

--- Structural/Bridge/Program.cs
+++ Structural/Bridge/Program.cs
@@ -1,55 +1,69 @@
 ﻿using System;
 using System.Drawing;
 using System.Security.Cryptography.X509Certificates;
+
 interface IColor
 {
     string GetColor();
 }
+
 class Red : IColor
 {
     public string GetColor() => "Red";
 }
+
 class Blue : IColor
 {
     public string GetColor() => "Blue";
 }
+
 class Green : IColor
 {
     public string GetColor() => "Green";
 }
+
 abstract class Shape
 {
     protected IColor color;
+    
     public Shape(IColor color)
     {
         this.color = color;
     }
+    
     public abstract void Draw();
 }
+
 class Circle : Shape
 {
     public Circle(IColor color) : base(color) { }
+    
     public override void Draw()
     {
         Console.WriteLine($"Drawing a {color.GetColor()} circle");
     }
 }
+
 class Square : Shape
 {
     public Square(IColor color) : base(color) { }
+    
     public override void Draw()
     {
         Console.WriteLine($"Drawing a {color.GetColor()} square");
     }
 }
+
 class Triangle : Shape
 {
     public Triangle(IColor color) : base(color) { }
+    
     public override void Draw()
     {
         Console.WriteLine($"Drawing a {color.GetColor()} triangle");
     }
 }
+
 class Program
 {
     static void Main(string[] args)
@@ -59,6 +73,7 @@ class Program
         Shape BlueTriangle = new Triangle(new Blue());
         Shape BlueSquare = new Square(new Blue());
         Shape GreenCircle = new Circle(new Green());
+        
         RedTriangle.Draw();
         GreenTriangle.Draw();
         BlueTriangle.Draw();
